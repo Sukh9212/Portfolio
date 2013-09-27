0db69213df7c71caef192c7a1342039ced8a9247
Sukhdeep Singh <sukhdeepsaini12@gmail.com>
2013-09-27 16:14:36 -0400

    Create services.php

---
+++ services.php
@@ -0,0 +1,47 @@
+<?php
+	include_once 'header.php';
+	?>
+	<!--
+	File Name: Services.php
+	Author's Name: Sukhdeep Singh
+	Website Name: My Portfolio
+	Desc : This is Services.php file that shows the services that I offer.
+	-->
+	 <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+	<h1>Services</h1>
+	<hr class="featurette-divider">
+	<h2 class="service_tagline">Potential Coding on these Platforms:</h2>
+	 <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="img/web.jpg" width="180" height="180" alt="Web image">
+          <h2>Web Development</h2>
+          <p>Web is the most alive platform for Developers and Designers. I specialize in Languages such as HTML5, CSS3, PHP, JavaScript, jQuery etc.
+          	With some designing abilities I can build basic websites to content managed websites using php with database connectivity as a backhand for the page content.
+          	I can build Sites with respnosive design adapting to various viewport sizes and devices which is the essential for any website.</p>
+          <p><a class="btn btn-default" href="projects.php#ab">View Work &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        
+        <div class="col-lg-4">
+          <img class="img-circle" src="img/windows8.jpg" width="180" height="180" alt="Windows 8.1 apps image">
+          <h2>App Development(Android, Windows 8,8.1)</h2>
+          <p>Still in the learning phase for developing apps for well Established app market such as android &amp; ios and Developing market such as Windows 8, Windows 8.1, Windows phone 8 etc.
+          	I've done some independent study on Android app development. Since it is Java so if given a chance I can also work to develop android apps and similarly with Windows apps
+          	I've done independent study on developing apps and I've Used C# as programming language. For Windows app I've also Used HTML5, CSS3 and JavaScript technologies.</p>
+        </div><!-- /.col-lg-4 -->
+        
+        <div class="col-lg-4">
+          <img class="img-circle" src="img/java.jpg" width="180" height="180" alt="Java image">
+          <h2>Software Development</h2>
+          <p>Expertise in the Java Programming language and in learning phase of C# progamming language. With these two Most powerful 
+          	languages in the pocket, I can build powerful standalone desktop applications. I've build a Java Chat Server using ofcourse
+          	java Which uses rich cores of the language and utilizes Swing, Multithreading, RMI and the most importantly Socket Programming.</p>
+          <p><a class="btn btn-default" href="projects.php#jchat">View Work &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+	
+	<?php
+    include_once 'footer.php';
+    ?>
