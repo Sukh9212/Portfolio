ca7bb2883ce83d0ff3013725243b6949eab4dd61
Sukhdeep Singh <sukhdeepsaini12@gmail.com>
2013-09-27 16:13:44 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!--
+	File Name: footer.php
+	Author's Name: Sukhdeep Singh
+	Website Name: My Portfolio
+	Desc : This is a footer file that includes all the footer information which is required to be consistent throughout the site.
+	-->
+<hr class="featurette-divider">
+<!-- FOOTER -->
+<footer>
+	<p class="pull-right">
+		<a href="#">Back to top</a>
+	</p>
+	<p>
+		&copy;Sukhdeep Singh 2013 All Rights Reserved &middot; <a href="legal.php#privacy">Privacy</a> &middot; <a href="legal.php#terms">Terms</a>
+	</p>
+</footer>
+
+</div><!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/holder.js"></script>
+</body>
+</html>
